a1f88751a5cea4f2d56e4429ed0d66d90c6dcbb8
SEBASTIAN RESTREPO RUIZ <sebastianrestreporuiz@gmail.com>
2026-02-23 20:24:35 -0500

    Create

---
+++ Logi/Taller 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Ingrese un número:");
+int numero = int.Parse(Console.ReadLine());
+if (numero % 2 == 0)
+{
+    Console.WriteLine("Es par");
+}
+else
+{
+    Console.WriteLine("Es impar");
+}
