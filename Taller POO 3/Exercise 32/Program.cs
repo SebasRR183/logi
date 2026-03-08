ce3de25924da726c0c177defce01e34ada504dbb
SEBASTIAN RESTREPO RUIZ <sebastianrestreporuiz@gmail.com>
2026-03-08 17:09:04 -0500

    creat blank solution

---
+++ Taller POO 3/Exercise 32/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exercise32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el tamaño del rombo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                Console.Write("#");
+
+                if (i > 1)
+                {
+                    for (int j = 1; j <= 2 * (i - 1) - 1; j++)
+                        Console.Write(" ");
+
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                Console.Write("#");
+
+                if (i > 1)
+                {
+                    for (int j = 1; j <= 2 * (i - 1) - 1; j++)
+                        Console.Write(" ");
+
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taller POO 3/Exercise 33/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Exercise33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese m: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese p: ");
+            int p = int.Parse(Console.ReadLine());
+
+            int[,] A = new int[m, n];
+            int[,] B = new int[n, p];
+            int[,] C = new int[m, p];
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    A[i, j] = (i + 1) * j;
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < p; j++)
+                    B[i, j] = (j + 1) * i;
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < p; j++)
+                    for (int k = 0; k < n; k++)
+                        C[i, j] += A[i, k] * B[k, j];
+
+            Console.WriteLine("\n*** A ***");
+            Mostrar(A);
+
+            Console.WriteLine("\n*** B ***");
+            Mostrar(B);
+
+            Console.WriteLine("\n*** C ***");
+            Mostrar(C);
+
+            Console.ReadKey();
+        }
+
+        static void Mostrar(int[,] matriz)
+        {
+            int filas = matriz.GetLength(0);
+            int columnas = matriz.GetLength(1);
+
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                    Console.Write(matriz[i, j] + " ");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
